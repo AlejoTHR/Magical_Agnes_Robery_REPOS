4027531b9902f5a07ac9281586c43cb1a46c2c14
April <pol.lopez@estudiant.enti.cat>
2026-05-31 20:41:20 +0200

    SomewhatFixedAudio

--- Assets/Scripts/Main Menu/HoveringButtons.cs
+++ Assets/Scripts/Main Menu/HoveringButtons.cs
@@ -5,7 +5,7 @@ public class MenuButtonAudio : MonoBehaviour, ISelectHandler, IDeselectHandler, 
 {
     [Header("Audio")]
     public AudioClip hoverSound;
-    public AudioClip hoverSelcectSound;
+    public AudioClip hoverSelcectSound; // This acts as your click sound
     public AudioSource hoverSource;
 
     [Header("Scaling Animation")]
@@ -58,27 +58,30 @@ public class MenuButtonAudio : MonoBehaviour, ISelectHandler, IDeselectHandler, 
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        // Add "EventSystem.current != null" to the check
         if (EventSystem.current != null && EventSystem.current.currentSelectedGameObject == gameObject)
         {
             EventSystem.current.SetSelectedGameObject(null);
         }
     }
+
     public void OnSelect(BaseEventData eventData)
     {
         if (!isSelected)
         {
             isSelected = true;
-            hoverSource.clip = hoverSound;  
+            hoverSource.clip = hoverSound;
 
             // Check all three possible Managers
-            if (MainMenu.Instance != null) {
+            if (MainMenu.Instance != null)
+            {
                 hoverSource.Play();
             }
-            else if (MenuPausa.Instance != null) {
+            else if (MenuPausa.Instance != null)
+            {
                 hoverSource.Play();
             }
-            else if (StartMenuManager.Instance != null){
+            else if (StartMenuManager.Instance != null)
+            {
                 hoverSource.Play();
             }
 
@@ -94,19 +97,29 @@ public class MenuButtonAudio : MonoBehaviour, ISelectHandler, IDeselectHandler, 
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        hoverSource.clip = hoverSelcectSound;
+        // 1. Force stop the hover sound if it's currently playing to clear the channel
+        if (hoverSource.isPlaying)
+        {
+            hoverSource.Stop();
+        }
+
+        // 2. Play the click sound using PlayOneShot so it takes instant priority
+        if (hoverSelcectSound != null)
+        {
+            hoverSource.PlayOneShot(hoverSelcectSound);
+        }
 
         // Check all three possible Managers
-        if (MainMenu.Instance != null){
-            hoverSource.Play();
+        if (MainMenu.Instance != null)
+        {
             MainMenu.Instance.UI_PlayClick();
         }
-        else if (MenuPausa.Instance != null){
-            hoverSource.Play();
+        else if (MenuPausa.Instance != null)
+        {
             MenuPausa.Instance.UI_PlayClick();
         }
-        else if (StartMenuManager.Instance != null){
-            hoverSource.Play();
+        else if (StartMenuManager.Instance != null)
+        {
             StartMenuManager.Instance.UI_PlayClick();
         }
     }
